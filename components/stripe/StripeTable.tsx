3ece09b8277fcd59a47e727728f0f604ba09e414
Arconte112 <rainiercf66@gmail.com>
2024-12-06 21:51:40 -0400

    Update StripePricingTable component: Replace hardcoded Stripe keys with new values for pricing table ID and publishable key. This change ensures the component is using the latest configuration for improved functionality.

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -35,8 +35,8 @@ const StripePricingTable = ({ user }: Props) => {
   return (
     <div className='flex flex-1 flex-col w-full'>
       <stripe-pricing-table
-          pricing-table-id="prctbl_1P0TL0C3ic5Sd20TGpWOU2Fi"
-          publishable-key="pk_live_51P0SikC3ic5Sd20T9QRaRKIkqy8l951LDgeOxcP24ZRXHnQzjnOFM7tfhsYdWksn1wNBdejJzvaxXGq0yRAxm14A00Py0XreGk"
+          pricing-table-id="prctbl_1QRL0tKiDfzKHeikcchcm4wh"
+          publishable-key="pk_live_51QQxSEKiDfzKHeikOfc99SL6ITjFkMDU9xjjitEPFvklmzz43Uark0qjrs84V36G7QjYsdMp6xdUAW7TEqagQ2Nh00MNaTymII"
           client-reference-id={user.id}
           customer-email={user.email}
       >
